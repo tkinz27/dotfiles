f837692d78575d92649d5032ebea3afae7f70604
Tony Kinsley <tkinsley.9@gmail.com>
2019-05-18 13:50:24 -0700

    making a bit more resilient

--- .install_dotfiles.sh
+++ .install_dotfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script just sets up https://github.com/tkinz27/dotfiles repo as a bare repo in the home directory.
 # It requires git to already be installed.
@@ -8,24 +8,46 @@
 # curl -sL https://api.github.com/repos/tkinz27/dotfiles/contents/.install_dotfiles.sh \
 #   | jq -r .content | base64 -d | sh -
 
-git clone --bare https://github.com/tkinz27/dotfiles $HOME/.files
+function install_ubuntu {
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo add-apt-repository ppa:git-core/ppa
 
-dotfiles() {
-    git --git-dir=$HOME/.files/ --work-tree=$HOME $@
+    sudo apt update
+
+    sudo apt install -y git neovim zsh
 }
 
-# first lets backup any existing dotfiles
-mkdir -p $HOME/.files.bak
-dotfiles checkout 2>&1 > /dev/null
-if [ $? = 0 ]; then
-    echo "Checked out dotfiles.";
-else
-    echo "Backing up pre-existing dotfiles.";
-    dotfiles checkout 2>&1 | egrep -v "^\w" | awk '{print $1}' | xargs -I{} rm {}
-fi
-dotfiles checkout
-dotfiles config status.showUntrackedFiles no
-dotfiles submodule init
-dotfiles submodule update
+if [[ ! -d ~/.files ]]; then
+    git clone --bare https://github.com/tkinz27/dotfiles $HOME/.files
 
-echo "chsh -s /usr/bin/zsh $(whoami)"
+    dotfiles() {
+        git --git-dir=$HOME/.files/ --work-tree=$HOME $@
+    }
+
+    # first lets backup any existing dotfiles
+    mkdir -p $HOME/.files.bak
+    dotfiles checkout 2>&1 > /dev/null
+    if [ $? = 0 ]; then
+        echo "Checked out dotfiles.";
+    else
+        echo "Backing up pre-existing dotfiles.";
+        dotfiles checkout 2>&1 | egrep -v "^\w" | awk '{print $1}' | xargs -I{} rm {}
+    fi
+    dotfiles checkout
+    dotfiles config status.showUntrackedFiles no
+    dotfiles submodule init
+    dotfiles submodule update
+else
+    echo "dotfiles already installed? skipping..."
+fi
+
+if [[ $(uname -s) == "Linux" ]]; then
+    id=$(cat /etc/os-release | grep ^ID= | cut -d'=' -f 2)
+    if [[ "$id" == "ubuntu" ]]; then
+        install_ubuntu
+    fi
+elif [[ $(uname -s) == "Darwin" ]]; then
+    install_mac
+fi
+
+chsh -s /usr/bin/zsh $(whoami)
