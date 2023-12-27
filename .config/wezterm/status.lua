9c5009f18cea76fe6faf4286a00abbe53228381c
Tony Kinsley <tony.kinsley@braincorp.com>
2023-12-27 10:28:36 -0800

    wezterm status func

---
+++ .config/wezterm/status.lua
@@ -0,0 +1,97 @@
+local wezterm = require('wezterm')
+
+local M = {}
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+
+local function contract_path(path)
+  local home = os.getenv('HOME')
+  if home then
+    path = path:gsub(home, '~')
+  end
+  return path
+end
+
+M.status_func = function(window, pane)
+  -- Each element holds the text for a cell in a "powerline" style << fade
+  local cells = {}
+
+  -- Figure out the cwd and host of the current pane.
+  -- This will pick up the hostname for the remote host if your
+  -- shell is using OSC 7 on the remote host.
+  local cwd_uri = pane:get_current_working_dir()
+  if cwd_uri then
+    cwd_uri = cwd_uri:sub(8)
+    local slash = cwd_uri:find('/')
+    local cwd = ''
+    local hostname = ''
+    if slash then
+      hostname = cwd_uri:sub(1, slash - 1)
+      -- Remove the domain name portion of the hostname
+      local dot = hostname:find('[.]')
+      if dot then
+        hostname = hostname:sub(1, dot - 1)
+      end
+      -- and extract the cwd from the uri
+      cwd = cwd_uri:sub(slash)
+      local contracted_cwd = contract_path(cwd)
+
+      table.insert(cells, contracted_cwd)
+      if hostname ~= '' then
+        table.insert(cells, hostname)
+      end
+    end
+  end
+
+  -- Set UTC Datetime
+  local date = wezterm.strftime_utc('%Y-%m-%d %H:%M UTC')
+  local tz = wezterm.strftime(' (%z)')
+  table.insert(cells, date .. tz)
+
+  -- An entry for each battery (typically 0 or 1 battery)
+  for _, b in ipairs(wezterm.battery_info()) do
+    table.insert(cells, string.format('%.0f%%', b.state_of_charge * 100))
+  end
+
+  -- Color palette for the backgrounds of each cell
+  local colors = {
+    "#82aaff",
+    "#2f334d",
+    "#2f334d",
+    "#222436",
+  }
+
+  -- Foreground color for the text across the fade
+  local text_fg = "#c8d3f5"
+
+  -- The elements to be formatted
+  local elements = {}
+  -- How many cells have been formatted
+  local num_cells = 0
+
+  -- Translate a cell into elements
+  local function push(text, is_last)
+    local cell_no = num_cells + 1
+    table.insert(elements, { Foreground = { Color = text_fg } })
+    table.insert(elements, { Background = { Color = colors[cell_no] } })
+    table.insert(elements, { Text = ' ' .. text .. ' ' })
+    if not is_last then
+      table.insert(elements, { Foreground = { Color = colors[cell_no + 1] } })
+      table.insert(elements, { Text = SOLID_LEFT_ARROW })
+    end
+    num_cells = num_cells + 1
+  end
+
+  while #cells > 0 do
+    local cell = table.remove(cells, 1)
+    push(cell, #cells == 0)
+  end
+
+  window:set_left_status("")
+  window:set_right_status(wezterm.format(elements))
+end
+
+return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,21 +6,25 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-  config.color_scheme = 'tokyonight'
+config.color_scheme = 'tokyonight'
+
+wezterm.on('update-status', function(window, pane)
+  require('status').status_func(window, pane)
+end)
 
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
-  { key = 'a', mods = 'LEADER|CTRL', action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' }},
+  { key = 'a', mods = 'LEADER|CTRL',  action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' } },
 
   -- split panes
-  { key = '|', mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }},
-  { key = '-', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }},
+  { key = '|', mods = 'LEADER|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = '-', mods = 'LEADER',       action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
 
-  -- 
-  { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left'},
-  { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down'},
-  { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up'},
-  { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right'},
+  --
+  { key = 'h', mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Left' },
+  { key = 'j', mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Up' },
+  { key = 'l', mods = 'LEADER',       action = wezterm.action.ActivatePaneDirection 'Right' },
 }
 
 config.mouse_bindings = {
