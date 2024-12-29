9e14c6cdc5bff116885c1ce7e505d596f95c101a
Tony Kinsley <tkinsley.9@gmail.com>
2024-12-29 09:26:54 -0800

    updating

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -8,7 +8,10 @@ return {
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-      provider = 'copilot',
+      provider = 'gemini',
+      gemini = {
+        model = 'gemini-2.0-flash-exp',
+      },
       vendors = {
         ---@type AvanteProvider
         ollama = {
